e63afb66fb0790df77cb5d9fe073c004e046c522
lion <win2key@gmail.com>
2023-05-15 00:02:03 +0300

    implemenation of simple socks5 without dependencies

--- common.go
+++ common.go
@@ -1,11 +1,17 @@
 package jproxy
 
 import (
+	"encoding/binary"
 	"io"
 	"log"
 	"net"
+	"strconv"
+	"sync"
+)
 
-	"golang.org/x/net/proxy"
+const (
+	socksVer5       = 5
+	socksCmdConnect = 1
 )
 
 func RunProxy(address string) {
@@ -14,7 +20,7 @@ func RunProxy(address string) {
 		log.Fatal("Failed to create listener:", err)
 	}
 
-	log.Println("Jproxy listening on ", address)
+	log.Println("Jproxy listening on", address)
 
 	for {
 		conn, err := listener.Accept()
@@ -30,29 +36,96 @@ func RunProxy(address string) {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	socks5Conn, err := proxy.SOCKS5("tcp", "", nil, proxy.Direct)
+	// Read the handshake message
+	buf := make([]byte, 2)
+	_, err := io.ReadFull(conn, buf)
 	if err != nil {
-		log.Println("Failed to create proxy: ", err)
+		log.Println("failed to read handshake:", err)
+		return
+	}
+	if buf[0] != socksVer5 {
+		log.Println("invalid SOCKS version:", buf[0])
 		return
 	}
 
-	target, err := socks5Conn.Dial("tcp", conn.RemoteAddr().String())
+	// Read the authentication methods
+	nmethods := int(buf[1])
+	methods := make([]byte, nmethods)
+	_, err = io.ReadFull(conn, methods)
 	if err != nil {
-		log.Println("Failed to connect to target: ", err)
+		log.Println("failed to read methods:", err)
+		return
+	}
+
+	// Send the handshake response
+	_, err = conn.Write([]byte{socksVer5, 0})
+	if err != nil {
+		log.Println("failed to write handshake response:", err)
+		return
+	}
+
+	// Read the request
+	buf = make([]byte, 4)
+	_, err = io.ReadFull(conn, buf)
+	if err != nil {
+		log.Println("failed to read request:", err)
+		return
+	}
+	if buf[0] != socksVer5 {
+		log.Println("invalid SOCKS version:", buf[0])
+		return
+	}
+	if buf[1] != socksCmdConnect {
+		log.Println("unsupported command:", buf[1])
+		return
+	}
+
+	// Read the target address and port
+	addr, err := readAddr(conn)
+	if err != nil {
+		log.Println("failed to read address:", err)
+		return
+	}
+	target, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Println("failed to connect to target:", err)
 		return
 	}
 	defer target.Close()
 
-	done := make(chan struct{})
-	go func() {
-		io.Copy(target, conn)
-		close(done)
-	}()
+	// Send the response
+	_, err = conn.Write([]byte{socksVer5, 0, 0, 1, 0, 0, 0, 0, 0, 0})
+	if err != nil {
+		log.Println("failed to write response:", err)
+		return
+	}
 
+	// Relay data
+	var wg sync.WaitGroup
+	wg.Add(2)
 	go func() {
-		io.Copy(conn, target)
-		close(done)
+		defer wg.Done()
+		_, _ = io.Copy(target, conn)
 	}()
+	go func() {
+		defer wg.Done()
+		_, _ = io.Copy(conn, target)
+	}()
+	wg.Wait()
+}
 
-	<-done
+func readAddr(conn net.Conn) (string, error) {
+	buf := make([]byte, 4)
+	_, err := io.ReadFull(conn, buf)
+	if err != nil {
+		return "", err
+	}
+	ip := net.IP(buf)
+	buf = make([]byte, 2)
+	_, err = io.ReadFull(conn, buf)
+	if err != nil {
+		return "", err
+	}
+	port := binary.BigEndian.Uint16(buf)
+	return net.JoinHostPort(ip.String(), strconv.Itoa(int(port))), nil
 }
